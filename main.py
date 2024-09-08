4737dde2ea9866e95c96789ff219004a1e24793a
Aitor <aitorgaillard99@gmail.com>
2024-09-08 11:24:52 +0200

    load data to table from database

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QGridLayout, QLabel, QWidget, \
-    QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget
+    QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget, \
+    QTableWidgetItem, QDialog, QVBoxLayout
 import sys
+import sqlite3
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(500)
+
+        layout = QVBoxLayout()
+
+        # create widgets
+
 
 
 class MainWindow(QMainWindow):
@@ -15,6 +30,7 @@ class MainWindow(QMainWindow):
 
         # create submenus
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -25,14 +41,30 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("ID", "NAME", "COURSE", "MOBILE"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
+    # populate table with database data
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        # loop for every row and then every row item
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number,
+                                   QTableWidgetItem(str(data)))
+        connection.close()
 
+    # generates an Insert Dialog that will display a new dialog to add the data
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
 
 # display app
 app = QApplication(sys.argv)
 main_window = MainWindow()
 main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
