84b3b7c79bb339ad389480f25ea8d6f320aec689
Aitor <aitorgaillard99@gmail.com>
2024-09-08 16:00:52 +0200

    add student function added

--- main.py
+++ main.py
@@ -11,18 +11,49 @@ class InsertDialog(QDialog):
         super().__init__()
         self.setWindowTitle("Insert Student Data")
         self.setFixedWidth(300)
-        self.setFixedHeight(500)
+        self.setFixedHeight(200)
 
         layout = QVBoxLayout()
 
         # create widgets
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
 
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        self.phone_number = QLineEdit()
+        self.phone_number.setPlaceholderText("123456789")
+        layout.addWidget(self.phone_number)
+
+        button = QPushButton("Add Student")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.phone_number.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) "
+                       "values(?, ?, ?)", (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setFixedWidth(800)
+        self.setFixedHeight(500)
 
         # create main menu
         file_menu_item = self.menuBar().addMenu("&File")
@@ -61,6 +92,7 @@ class MainWindow(QMainWindow):
     def insert(self):
         dialog = InsertDialog()
         dialog.exec()
+
 
 # display app
 app = QApplication(sys.argv)
